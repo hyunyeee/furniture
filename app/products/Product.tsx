df735cd11f807c70a1ce078f48e156445db00706
hyunyeee <5004ohy@naver.com>
2025-06-06 16:29:30 +0900

    feat: 상품 리스트 조회 기능 구현

---
+++ app/products/Product.tsx
@@ -0,0 +1,34 @@
+import { CategoryProduct } from "@/types/products";
+import Link from "next/link";
+
+const Product = ({
+  id,
+  name,
+  description,
+  price,
+  categoryName,
+  // image,
+}: CategoryProduct) => {
+  return (
+    <Link
+      href={`/products/${id}`}
+      className="rounded-xl border-1 border-gray-100 px-6 py-4"
+    >
+      {/*{image && <img src={image} alt="상품 이미지" />}*/}
+      <div className="mb-2 h-[200px] w-[200px] bg-gray-100 text-center">
+        이미지
+      </div>
+      <div className="mb-4 flex items-center justify-between">
+        <p className="font-semibold">{name}</p>
+        <p className="inline rounded-md bg-slate-500 px-2 py-1 text-sm text-white">
+          {categoryName}
+        </p>
+      </div>
+
+      <p>{description}</p>
+      <p className="font-semibold">{price}</p>
+    </Link>
+  );
+};
+
+export default Product;
