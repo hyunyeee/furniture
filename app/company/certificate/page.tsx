2d2f261db4496d312cb5915e6bb7f4e9210e1ab9
hyunyeee <5004ohy@naver.com>
2025-06-17 03:31:41 +0900

    feat: 인증서 조회 기능 구현

---
+++ app/company/certificate/page.tsx
@@ -0,0 +1,47 @@
+import { Certificate } from "@/types/company";
+
+import CertificatesCard from "@/app/components/company/CertificatesCard";
+
+async function getCertificate(): Promise<Certificate[]> {
+  const apiUrl = process.env.API_URL as string;
+  const response = await fetch(`${apiUrl}/certificates`);
+
+  if (!response.ok) {
+    throw new Error(`API 요청 실패: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  if (!Array.isArray(data)) {
+    console.error("API 응답이 배열이 아닙니다:", data);
+    return [];
+  }
+
+  return data;
+}
+
+export default async function CertificatePage() {
+  const certificates: Certificate[] = await getCertificate();
+
+  return (
+    <div className="my-[100px] flex h-screen flex-col justify-between px-20 pb-20">
+      <h1 className="text-center text-3xl font-semibold">인증서</h1>
+
+      {certificates.length === 0 ? (
+        <p>인증서 데이터가 존재하지 않습니다..</p>
+      ) : (
+        <div className="mt-[50px] flex flex-wrap justify-center gap-6 pb-[100px]">
+          {certificates.map(({ id, imageUrl, tag, description }) => (
+            <CertificatesCard
+              key={id}
+              id={id}
+              imageUrl={imageUrl}
+              tag={tag}
+              description={description}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
