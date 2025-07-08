062c5ad247c3d67446f1bf204418c1946b2adc7f
hyunyeee <5004ohy@naver.com>
2025-07-08 14:59:36 +0900

    design: 연혁 디자인 수정

--- app/components/company/HistoryCard.tsx
+++ app/components/company/HistoryCard.tsx
@@ -1,4 +1,5 @@
 import { Histories } from "@/types/company";
+import Image from "next/image";
 
 export default function HistoryCard({
   title,
@@ -7,11 +8,40 @@ export default function HistoryCard({
   imageUrl2,
 }: Histories) {
   return (
-    <div className="border-1 border-gray-700 p-4">
-      <h2 className="font-semibold">{title}</h2>
-      <p>{content}</p>
-      <p className="text-gray-700">{imageUrl1}</p>
-      <p className="text-gray-700">{imageUrl2}</p>
-    </div>
+    <li className="ms-4 mb-10">
+      <div className="absolute start-0 top-1.5 h-3 w-3 rounded-full border border-white bg-gray-200" />
+
+      <time className="mb-1 block text-sm leading-none font-medium text-gray-500">
+        {title}
+      </time>
+
+      <h3 className="mb-1 text-lg font-semibold text-gray-900">{title}</h3>
+      <p className="mb-3 text-sm whitespace-pre-line text-gray-700">
+        {content}
+      </p>
+
+      {(imageUrl1 || imageUrl2) && (
+        <div className="mt-2 flex flex-wrap gap-4">
+          {imageUrl1 && (
+            <Image
+              src={imageUrl1}
+              alt="연혁 이미지1"
+              width={240}
+              height={160}
+              className="rounded-md border border-gray-100 object-cover"
+            />
+          )}
+          {imageUrl2 && (
+            <Image
+              src={imageUrl2}
+              alt="연혁 이미지2"
+              width={240}
+              height={160}
+              className="rounded-md border border-gray-100 object-cover"
+            />
+          )}
+        </div>
+      )}
+    </li>
   );
 }
