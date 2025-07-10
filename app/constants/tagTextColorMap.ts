9e81b486caa9cba9f878fa6089d91d74578c6865
hyunyeee <5004ohy@naver.com>
2025-07-10 14:29:34 +0900

    design: 태그 색상 수정

--- app/constants/tagTextColorMap.ts
+++ app/constants/tagTextColorMap.ts
@@ -1,12 +1,14 @@
 export const tagTextColorMap: { [key: string]: string } = {
-  "S Series": "#7ece83",
-  "P Series": "#9ed1ff",
-  "DM Series": "#fdd9a0",
-  "DMC Series": "#f4bfbf",
-  "DMP Series": "#a2aacc",
-  "세미나 테이블": "#bb9161",
-  "열린 교실 책상": "#666ead",
-  "강연대, 반원테이블, 식탁 ": "#73af8c",
-  "가림판 (유)": "#c2e7f0",
-  "가림판 (무)": "#e2e2e2",
+  "S Series": "#5fb86a",
+  "P Series": "#73b7f5",
+  "DM Series": "#f7be68",
+  "DMC Series": "#ea9c9c",
+  "DMP Series": "#7f87ba",
+  "세미나 테이블": "#a27445",
+  "열린 교실 책상": "#4c529d",
+  "강연대, 반원테이블, 식탁 ": "#53976f",
+  "가림판 (유)": "#9ddae9",
+  "가림판 (무)": "#bdbdbd",
 };
+
+export type TagTextColorKey = keyof typeof tagTextColorMap;
