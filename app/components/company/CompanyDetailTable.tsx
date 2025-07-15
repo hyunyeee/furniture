a3ddc45cdcff11246ba437b598898d76708e5099
hyunyeee <5004ohy@naver.com>
2025-07-15 16:59:46 +0900

    design: 회사 소개 페이지 디자인 수정

--- app/components/company/CompanyDetailTable.tsx
+++ app/components/company/CompanyDetailTable.tsx
@@ -21,31 +21,31 @@ export default async function DetailTable() {
     <table className="w-full table-auto border-collapse text-left text-sm">
       <tbody>
         <tr>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             회사명
           </th>
           <td className="border p-2">{companyName}</td>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             대표
           </th>
           <td className="border p-2">{ceo}</td>
         </tr>
         <tr>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             설립일
           </th>
           <td className="border p-2">{establishmentDate}</td>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             사업영역
           </th>
           <td className="border p-2">{businessArea}</td>
         </tr>
         <tr>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             사업장규모
           </th>
           <td className="border p-2">{scale}</td>
-          <th className="border bg-gray-100 p-2 font-medium text-gray-700">
+          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
             주거래처
           </th>
           <td className="border p-2">{mainClient}</td>
