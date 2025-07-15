236cffb86778cf915fd97f34b357e15d9f07b765
hyunyeee <5004ohy@naver.com>
2025-07-15 17:33:22 +0900

    design: 회사 소개 Table 디자인 수정

--- app/components/company/CompanyDetailTable.tsx
+++ app/components/company/CompanyDetailTable.tsx
@@ -18,39 +18,41 @@ export default async function DetailTable() {
   } = companyDetail;
 
   return (
-    <table className="w-full table-auto border-collapse text-left text-sm">
-      <tbody>
-        <tr>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            회사명
-          </th>
-          <td className="border p-2">{companyName}</td>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            대표
-          </th>
-          <td className="border p-2">{ceo}</td>
-        </tr>
-        <tr>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            설립일
-          </th>
-          <td className="border p-2">{establishmentDate}</td>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            사업영역
-          </th>
-          <td className="border p-2">{businessArea}</td>
-        </tr>
-        <tr>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            사업장규모
-          </th>
-          <td className="border p-2">{scale}</td>
-          <th className="border bg-gray-100 p-2 text-center font-medium text-gray-700">
-            주거래처
-          </th>
-          <td className="border p-2">{mainClient}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="overflow-hidden rounded-md border border-gray-200">
+      <table className="min-w-full table-auto text-left text-sm">
+        <tbody>
+          <tr className="border-b border-gray-200">
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              회사명
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{companyName}</td>
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              대표
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{ceo}</td>
+          </tr>
+          <tr className="border-b border-gray-200">
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              설립일
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{establishmentDate}</td>
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              사업영역
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{businessArea}</td>
+          </tr>
+          <tr>
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              사업장규모
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{scale}</td>
+            <th className="w-1/6 bg-gray-100 p-2 text-center font-semibold text-gray-700">
+              주거래처
+            </th>
+            <td className="w-2/6 p-2 text-gray-800">{mainClient}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
