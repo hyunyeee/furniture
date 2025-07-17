f76f3c7c8f56ee87bc8c206bad72cfaf6ff469a7
hyunyeee <5004ohy@naver.com>
2025-07-18 00:09:49 +0900

    design: 인증서 반응형 디자인 구현

--- app/(company)/company/certificate/page.tsx
+++ app/(company)/company/certificate/page.tsx
@@ -23,13 +23,13 @@ export default async function CertificatePage() {
   const certificates: Certificate[] = await getCertificate();
 
   return (
-    <div className="mt-[100px] flex flex-col justify-between px-20">
-      <h1 className="text-center text-3xl font-semibold">인증서</h1>
+    <div className="mx-auto mt-[100px] flex max-w-screen-xl flex-col justify-between px-4 sm:px-20">
+      <h1 className="text-center text-2xl font-semibold sm:text-3xl">인증서</h1>
 
       {certificates.length === 0 ? (
-        <p>인증서 데이터가 존재하지 않습니다..</p>
+        <p className="mt-10 text-center">인증서 데이터가 존재하지 않습니다..</p>
       ) : (
-        <div className="mt-[50px] flex flex-wrap justify-center gap-6 pb-[100px]">
+        <div className="mt-[50px] flex flex-wrap justify-center gap-3 pb-[100px] sm:gap-6">
           {certificates.map(({ id, imageUrl, tag, description }) => (
             <CertificatesCard
               key={id}
