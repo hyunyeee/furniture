000711379b45dee990ad497aedddb1444cfa2e5c
hyunyeee <5004ohy@naver.com>
2025-07-18 13:42:01 +0900

    design: 로그인, 회원가입 반응형 디자인 구현

--- components/auth/FormCard.tsx
+++ components/auth/FormCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function FormCard({ title, footer, children }: Props) {
   return (
-    <div className="mt-[60px] mb-[30px] flex w-[50%] max-w-[550px] flex-col justify-center gap-4 rounded-xl bg-white/40 px-6 py-6 shadow-lg backdrop-blur-md">
+    <div className="mx-auto mt-[60px] mb-[30px] flex w-full max-w-[550px] flex-col justify-center gap-4 rounded-xl bg-white/40 px-6 py-6 shadow-lg backdrop-blur-md sm:w-[80%] sm:max-w-[450px] md:w-[60%]">
       <h1 className="text-center text-2xl font-semibold">{title}</h1>
       <div>{children}</div>
       <Link
