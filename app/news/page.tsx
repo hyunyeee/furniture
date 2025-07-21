97c8bb1aacb90260ae0777bdff9ee2b86b92958a
hyunyeee <5004ohy@naver.com>
2025-07-21 16:30:26 +0900

    feat: 뉴스 페이지 구현

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,19 @@
-import Maintenance from "@/components/Maintenance";
+import { NewsCardProps } from "@/types/updates";
+import { getNews } from "@/lib/api/updates";
+import NewsCard from "@/components/company/NewsCard";
 
-export default function NewsPage() {
-  return <Maintenance />;
+export default async function NewsPage() {
+  const newsList: NewsCardProps[] = await getNews();
+
+  return (
+    <div className="my-[100px] flex flex-col justify-between gap-10">
+      <h1 className="text-center text-3xl font-semibold">뉴스</h1>
+
+      <div className="flex flex-wrap justify-center gap-6">
+        {newsList.map((news) => (
+          <NewsCard key={news.id} {...news} />
+        ))}
+      </div>
+    </div>
+  );
 }
